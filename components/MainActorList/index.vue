f42cbdead0bc591eb2534cbd9a7dda5291dea3d8
Dorynn <dolinha1k12@gmail.com>
2023-06-25 15:42:49 +0700

    update12

--- components/MainActorList/index.vue
+++ components/MainActorList/index.vue
@@ -2,7 +2,7 @@
     <FilmList id="mainActorList" :filmtype="filmtype" v-if="actors.length>0">
         <div class="row actorList" v-if="actors.length > 6">
           <div class="actorCard col-2"  v-for="item in actors.slice(0,6)" :key="item.id" >
-            <img :src="item!=null?`https://image.tmdb.org/t/p/w500${item.profile_path}`:require('~/assets/images/error.jpg')" alt="" style="object-fit: cover;"/>
+            <img :src="item.profile_path!=null?`https://image.tmdb.org/t/p/w500${item.profile_path}`:require('~/assets/images/error.jpg')" alt="" style="object-fit: cover;"/>
             <p class="m-0 text-center"> {{item?item.original_name:""}}</p>
             <strong>{{item.character}}</strong>
           </div>
